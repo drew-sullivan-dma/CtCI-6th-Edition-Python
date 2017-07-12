694babbe4ead4fdfaa5f4b24768b82b9887ce0ca
drew <drew.sullivan@decisiondesk.com>
2017-07-12 08:51:05 -0400

    Cleaning for nav

--- Chapter1/1_isUnique.py
+++ Chapter1/1_isUnique.py
@@ -7,8 +7,10 @@ def unique(string):
     if len(string) > 128:
         return False
 
-    char_set = [False for _ in range(128)]
+    # Create a set of 128 Falses (implemented as a list)
+    char_set = [False] * 128
     for char in string:
+        # get the unicode code point of the char ex. ord('a') == 97
         val = ord(char)
         if char_set[val]:
             # Char already found in string
